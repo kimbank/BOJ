c8104b3d614154e1f3f28883239b56da2691a578
Bank <87305109+kimbank@users.noreply.github.com>
2022-06-06 20:27:43 +0900

    02738 C++

---
+++ C++/02738 행렬 덧셈.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+#define FOR(x, n) for (int x = 0; x < n; ++ x)
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int N, M, d;
+    cin >> N >> M;
+    vector<vector<int>> v(N);
+    
+    FOR(i, N) {
+        FOR(j, M) {
+            cin >> d;
+
+            v[i].push_back(d);
+        }
+    }
+
+    FOR(i, N) {
+        FOR(j, M) {
+            cin >> d;
+            cout << v[i][j] + d << " ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
