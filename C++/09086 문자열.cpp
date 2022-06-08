52bdf7b2892d1067d96babb8934da6649d3dee3d
Bank <87305109+kimbank@users.noreply.github.com>
2022-06-08 15:19:50 +0900

    9086: Bronze V

---
+++ C++/09086 문자열.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+#define FOR(x, n) for(int x = 0; x < n; ++ x)
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    FOR(i, n) {
+        string str;
+        cin >> str;
+
+        cout << str[0] << str[str.size() - 1] << "\n";
+    }
+    
+    return 0;
+}
